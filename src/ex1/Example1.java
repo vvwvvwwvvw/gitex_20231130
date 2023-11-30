937cf29f928d1156475d50538337e9004f852355
vvwvvwwvvw <wjddnr3304@gmail.com>
2023-11-30 10:11:03 +0900

    pint

--- src/ex1/Example1.java
+++ src/ex1/Example1.java
@@ -3,7 +3,8 @@ package ex1;
 public class Example1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("오늘은 목요일입니다");
+		System.out.println("오늘은 날씨가 춥습니다");
 
 	}
 
